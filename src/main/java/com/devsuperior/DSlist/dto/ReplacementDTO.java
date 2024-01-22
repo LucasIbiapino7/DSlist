e31cdaf87f3ce92081f34f369d85eda33dc2cd6f
Lucas Ibiapino <lucas.ibiapino07@gmail.com>
2024-01-22 11:05:21 -0300

    Move Position completed

---
+++ src/main/java/com/devsuperior/DSlist/dto/ReplacementDTO.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DSlist.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class ReplacementDTO {
+    private int sourceIndex;
+    private int destinationIndex;
+}
